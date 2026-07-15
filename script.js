0e7da10d2b62afff78cb213e83d02d03a176cf86
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-07-15 08:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let prices=document.querySelectorAll('.price');
+console.log(prices)
 
-const getSum = () => {
-//Add your code here
+// const getSum = () => {
+// //Add your code here
   
-};
+// };
 
-getSumBtn.addEventListener("click", getSum);
+// getSumBtn.addEventListener("click", getSum);
 
